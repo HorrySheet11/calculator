6a00d66eba7b508ac19f1d9187b5dfdd2c23c723
HorrySheet11 <marinnathan11@gmail.com>
2025-10-15 11:42:01 +0800

    added functions for multiple buttons
    added functions for all numbers, clear, delete

--- script.js
+++ script.js
@@ -13,4 +13,36 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }   
+const display = document.querySelector('.display');
+function clearDisplay() {
+    display.textContent = '0';
+}
 
+function updateDisplay(value) {
+    if (display.textContent === '0') {
+        display.textContent = value;
+    } else {
+        display.textContent += value;
+    }
+}
+
+function deleteText(){
+    display.textContent = display.textContent.slice(0, -1);
+}
+
+let num1 = null;
+let num2 = null;
+let operator = null;
+
+function calculate() {
+    num2 = parseFloat(display.textContent);
+    if (operator === '+') {
+        display.textContent = add(num1, num2);
+    } else if (operator === '-') {
+        display.textContent = subtract(num1, num2);
+    } else if (operator === '*') {
+        display.textContent = multiply(num1, num2);
+    } else if (operator === '/') {
+        display.textContent = divide(num1, num2);
+    }
+}
